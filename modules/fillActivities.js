9f504049fd9b4acc7862a8f33ce863e32d5e5815
rmiyoyo <miyoyoraphael@gmail.com>
2023-08-19 21:28:07 +0300

    Fix completed tasks logic

--- modules/fillActivities.js
+++ modules/fillActivities.js
@@ -1,37 +1,36 @@
 const fillActivities = (activities) => {
   const activitiesArea = document.querySelector('.todo-container');
-  const activityContainer = [];
+
   for (let a = 0; a < activities.length; a += 1) {
-    activityContainer[a] = document.createElement('div');
-    activityContainer[a].classList.add('hrzntl', 'activity-area');
-    for (let b = a; b < activities.length; b += 1) {
-      if (activities[b].index < activities[a].index) {
-        [activities[a], activities[b]] = [activities[b], activities[a]];
-      }
-    }
+    const activityContainer = document.createElement('div');
+    activityContainer.classList.add('hrzntl', 'activity-area');
+
+    // Sort activities based on index
+    activities.sort((activityA, activityB) => activityA.index - activityB.index);
+
     if (activities[a].description !== '') {
-      activityContainer[a].innerHTML = `<div class = "activity-description-area hrzntl">
-                                <input class = "confirmChoice${activities[a].index} mark-activity" type = "checkbox">
-                                  <p class = "d${activities[a].index} activity-details">${activities[a].description}</p>
-                                </div>
-                                <div class = "d${activities[a].index} display-periods period-container flex-column"><span class = "period"></span><span class = "period"></span><span class = "period"></span></div>
-                                <i class="d${activities[a].index}  fa-regular fa-pen-to-square modify"></i>
-                                <i id = "d${activities[a].index}" class="fa-regular fa-trash-can bin"></i>
-                                `;
-      activitiesArea.appendChild(activityContainer[a]);
-    } else if (activities[a].description !== '' && activities[a].completed) {
-      activityContainer[a].innerHTML = `<div class = "activity-description-area hrzntl">
-      <input class = "confirmChoice${activities[a].index} mark-activity" type = "checkbox" checked>
-      <p class = "d${activities[a].index} activity-details">${activities[a].description}</p>
-      </div>
-      <div class = "d${activities[a].index} display-periods period-container flex-column"><span class = "period"></span><span class = "period"></span><span class = "period"></span></div>
-      <i class="d${activities[a].index}  fa-regular fa-pen-to-square modify"></i>
-      <i id = "d${activities[a].index}" class="fa-regular fa-trash-can bin"></i>
+      const isChecked = activities[a].completed ? 'checked' : '';
+      activityContainer.innerHTML = `
+        <div class="activity-description-area hrzntl">
+          <input class="confirmChoice${activities[a].index} mark-activity" type="checkbox" ${isChecked} data-checked="${activities[a].completed}">
+          <p class="d${activities[a].index} activity-details">${activities[a].description}</p>
+        </div>
+        <div class="d${activities[a].index} display-periods period-container flex-column">
+          <span class="period"></span><span class="period"></span><span class="period"></span>
+        </div>
+        <i class="d${activities[a].index} fa-regular fa-pen-to-square modify"></i>
+        <i id="d${activities[a].index}" class="fa-regular fa-trash-can bin"></i>
       `;
-      activitiesArea.appendChild(activityContainer[a]);
-      document.querySelector(`.d${activities[a].index}`).style.textDecoration = 'line-through';
+
+      if (activities[a].completed) {
+        activityContainer.querySelector(`.d${activities[a].index}`).style.textDecoration = 'line-through';
+      }
+
+      activityContainer.querySelector(`.d${activities[a].index}`).contentEditable = true;
+      activitiesArea.appendChild(activityContainer);
+      localStorage.setItem(`confirmChoice${activities[a].index}`, activities[a].completed);
     }
-    document.querySelector(`.d${activities[a].index}`).contentEditable = true;
   }
 };
+
 export default fillActivities;
